37d22c094d1945efda0549a762fae9ff26fd8a23
kalyan <kalyanraj3003@gmail.com>
2026-04-09 17:30:44 +0530

    fix: add HttpServletRequest param to logout endpoint

--- src/main/java/com/wings1/controller/AuthController.java
+++ src/main/java/com/wings1/controller/AuthController.java
@@ -2,6 +2,7 @@ package com.wings1.controller;
 
 import com.wings1.dto.AuthRequest;
 import com.wings1.service.AuthService;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,7 +24,8 @@ public class AuthController {
     }
 
     @PostMapping("/logout")
-    public ResponseEntity<String> logout() {
+    public ResponseEntity<String> logout( HttpServletRequest request) {
+        // TODO: invalidate token heregi
         return ResponseEntity.ok("Logged out successfully");
     }
 }
